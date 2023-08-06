1f16820872055a847d59009a5b27443cf061d332
jishan1223 <141535212+jishan1223@users.noreply.github.com>
2023-08-06 08:13:56 +0545

    Add files via upload

---
+++ zipp.py
@@ -0,0 +1,11 @@
+'''a = [1, 2, 3, 4]
+lett = ['a', 'e', 'c', 'd']
+
+zipped = zip(a, lett)
+#type(zipped)
+print(list(zipped))
+print(type(zipped))'''
+
+
+square = {str(num) : num*num for num in range(1,11)}
+print(square)
